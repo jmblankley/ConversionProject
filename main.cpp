0230e7c7e185b8aa1fec0ffcf4de49d81c41761c
jmblankley <jmblankley@outlook.com>
2023-10-04 13:45:58 -0500

    pulled in binaryToHex function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
 Conversion Project
 
-Joshua Blankley, ...add names here
+Joshua Blankley, Aaron Jacobsen, ...
 
 October 2023
 
@@ -66,6 +66,53 @@ void hexToBinary(long valueToConvert)
 // Params: valueToConvert The binary value to convert to hexadecimal.
 void binaryToHex(long valueToConvert)
 {
+	// used for storing each digit from the binary number passed in by the user
+	long arrayofdigits[64];
+
+	// used for storing the remainder in first while loop
+	long remainder = 0;
+
+	// used for storing the number calculated by the four or eight binary digit number passed in by the user
+	long calculatednumber = 0;
+
+	// a and b used for iterating and functioning, and performing calculations in the while and for loops
+	long a = 1;
+
+	long b = 0;
+
+	// next three loops convert binary number to hexadecimal number
+	// loop performs the appropriate calculations for each binary digit and adds them together and stores them into the calculatednumber variable
+	// if user inputted an eight digit binary number will store both calculated numbers from the first and last four binary digits into the same calculatednumber variable
+	while (valueToConvert > 0)
+	{
+		remainder = valueToConvert % 2;
+		calculatednumber += remainder * a;
+		a = a * 2;
+		valueToConvert = valueToConvert / 10;
+	}
+
+	// reset a back to zero
+	a = 0;
+	// loop stores the calculated number from the first four binary digits or if user inputted eight binary digits the calculated numbers from first and last four digits seperately into array that contains the one or two number values needed for reading in the next loop to finally convert to the right hexadecimal value
+	while (calculatednumber != 0)
+	{
+		arrayofdigits[a] = calculatednumber % 16;
+		calculatednumber /= 16;
+		a++;
+	}
+	// for loop that, executes after converting binary number to hexadecimal number above, that will print hexadecimal value as correct corresponding hexadecimal number value if the converted value is less than nine and if the converted value is more than nine will print the correct corresponding hexadecimal letter or character value
+	for (b = a - 1; b >= 0; b--)
+	{
+		if (arrayofdigits[b] > 9)
+		{
+			cout << (char)(arrayofdigits[b] + 55);
+		}
+		else
+		{
+			cout << arrayofdigits[b];
+		}
+	}
+	cout << endl;
 }
 
 // Purpose: Prints the list of available commands.
